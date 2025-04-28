b971f1e5ec0d068b7f3ace675aaae8a2ab3276b1
xgyx <2731290559@qq.com>
2025-04-29 00:13:16 +0800

    基准测试，string结构支持

---
+++ redis/types.go
@@ -0,0 +1,86 @@
+package redis
+
+import (
+	bitcask "bitcask-go"
+	"encoding/binary"
+	"errors"
+	"time"
+)
+
+var (
+	ErrWrongTypeOperation = errors.New("WRONGTYPE Operation against a key holding the wrong kind of value")
+)
+
+type redisDataType = byte
+
+const (
+	String redisDataType = iota
+	Hash
+	Set
+	List
+	ZSet
+)
+
+type RedisDataStructure struct {
+	db *bitcask.DB
+}
+
+func NewRedisDataStructure(options bitcask.Options) (*RedisDataStructure, error) {
+	db, err := bitcask.Open(options)
+	if err != nil {
+		return nil, err
+	}
+
+	return &RedisDataStructure{db: db}, nil
+}
+
+// ---------------------String-------------------
+
+// value format:
+// +-------+--------+-----------+
+// | type  | expire |  payload  |
+// +-------+--------+-----------+
+
+func (rds *RedisDataStructure) Set(key []byte, ttl time.Duration, value []byte) error {
+	if value == nil {
+		return nil
+	}
+
+	buf := make([]byte, binary.MaxVarintLen64+1)
+	buf[0] = String
+	var index = 1
+	var expire int64 = 0
+	if ttl != 0 {
+		expire = time.Now().Add(ttl).UnixNano()
+	}
+	index += binary.PutVarint(buf[index:], expire)
+
+	encValue := make([]byte, index+len(value))
+	copy(encValue[:index], buf[:index])
+	copy(encValue[index:], value)
+
+	return rds.db.Put(key, encValue)
+}
+
+func (rds *RedisDataStructure) Get(key []byte) ([]byte, error) {
+	encValue, err := rds.db.Get(key)
+	if err != nil {
+		return nil, err
+	}
+
+	// 解码
+	dataType := encValue[0]
+	if dataType != String {
+		return nil, ErrWrongTypeOperation
+
+	}
+
+	var index = 1
+	expire, n := binary.Varint(encValue[index:])
+	index += n
+	if expire > 0 && time.Now().UnixNano() >= expire {
+		return nil, nil
+	}
+
+	return encValue[index:], nil
+}
---
+++ redis/types_test.go
@@ -0,0 +1,60 @@
+package redis
+
+import (
+	bitcask "bitcask-go"
+	"bitcask-go/utils"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRedisDataStructure_Get(t *testing.T) {
+	opts := bitcask.DefaultOptions
+	dir, _ := os.MkdirTemp("", "bitkcask-go-redis-get")
+	opts.DirPath = dir
+	rds, err := NewRedisDataStructure(opts)
+	assert.Nil(t, err)
+
+	err = rds.Set(utils.GetTestKey(1), 0, utils.RandomValue(100))
+	assert.Nil(t, err)
+
+	err = rds.Set(utils.GetTestKey(2), time.Second*5, utils.RandomValue(100))
+	assert.Nil(t, err)
+
+	val1, err := rds.Get(utils.GetTestKey(1))
+	assert.Nil(t, err)
+	assert.NotNil(t, val1)
+
+	val2, err := rds.Get(utils.GetTestKey(2))
+	assert.Nil(t, err)
+	assert.NotNil(t, val2)
+
+	_, err = rds.Get(utils.GetTestKey(3))
+	assert.Equal(t, bitcask.ErrKeyNotFound, err)
+}
+
+func TestRedisDataStructure_Type(t *testing.T) {
+	opts := bitcask.DefaultOptions
+	dir, _ := os.MkdirTemp("", "bitkcask-go-redis-type")
+	opts.DirPath = dir
+	rds, err := NewRedisDataStructure(opts)
+	assert.Nil(t, err)
+
+	err = rds.Del(utils.GetTestKey(21))
+	assert.Nil(t, err)
+
+	err = rds.Set(utils.GetTestKey(1), 0, utils.RandomValue(100))
+	assert.Nil(t, err)
+
+	typ, err := rds.Type(utils.GetTestKey(1))
+	assert.Nil(t, err)
+	assert.Equal(t, String, typ)
+
+	err = rds.Del(utils.GetTestKey(1))
+	assert.Nil(t, err)
+
+	_, err = rds.Get(utils.GetTestKey(1))
+	assert.Equal(t, bitcask.ErrKeyNotFound, err)
+}
